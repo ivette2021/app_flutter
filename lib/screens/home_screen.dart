331ff63fa772b3eda9e054db5c41b80d4be15351
ivette <iconstanzav1@gmail.com>
2024-04-13 23:19:15 -0400

    estilo de texto

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //es como el f12 del navegador
+    const fontSize30 = TextStyle(
+        fontSize: 30,
+        color: Colors
+            .white); //se creo una varibale para la fuente, para reutilizar
+
     return Scaffold(
         //esto prepara mi sitio es el fondo blanco
         appBar: AppBar(
@@ -18,9 +23,22 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.indigo.shade900,
         body: const Center(
           child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, //centrado en todos lados
             children: <Widget>[
-              Text('primera Linea'),
-              Text('Segunda Linea'),
+              Text(
+                'Primera Linea',
+                style: TextStyle(fontSize: 30, color: Colors.amber),
+              ),
+              Text(
+                'Primera Linea',
+                style: fontSize30, //llamamos la variable
+              ),
+              Text(
+                'Segunda Linea',
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 255, 98, 140)),
+              ),
             ],
           ),
         ));
