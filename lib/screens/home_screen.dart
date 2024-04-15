b4218a4d7eb3ba9b8288074ed4d8d2826eabdd52
ivette <iconstanzav1@gmail.com>
2024-04-14 23:01:45 -0400

    Boton flotante-FloatingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,41 +6,51 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //es como el f12 del navegador
+    //es como el f12 del navegadoryoutube
+
     const fontSize30 = TextStyle(
         fontSize: 30,
         color: Colors
             .white); //se creo una varibale para la fuente, para reutilizar
 
     return Scaffold(
-        //esto prepara mi sitio es el fondo blanco
-        appBar: AppBar(
-          //funciona sin const
-          backgroundColor: Colors.teal.shade400,
-          title: const Text('CounterApp'),
-          elevation: 30.0,
+      //esto prepara mi sitio es el fondo blanco
+      appBar: AppBar(
+        //funciona sin const
+        backgroundColor: Colors.teal.shade400,
+        title: const Text('CounterApp'),
+        elevation: 30.0,
+      ),
+      backgroundColor: Colors.indigo.shade900,
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //centrado en todos lados
+          children: <Widget>[
+            Text(
+              'Primera Linea',
+              style: TextStyle(fontSize: 30, color: Colors.amber),
+            ),
+            Text(
+              'Primera Linea',
+              style: fontSize30, //llamamos la variable
+            ),
+            Text(
+              'Segunda Linea',
+              style: TextStyle(
+                  fontSize: 20, color: Color.fromARGB(255, 255, 98, 140)),
+            ),
+          ],
         ),
-        backgroundColor: Colors.indigo.shade900,
-        body: const Center(
-          child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.center, //centrado en todos lados
-            children: <Widget>[
-              Text(
-                'Primera Linea',
-                style: TextStyle(fontSize: 30, color: Colors.amber),
-              ),
-              Text(
-                'Primera Linea',
-                style: fontSize30, //llamamos la variable
-              ),
-              Text(
-                'Segunda Linea',
-                style: TextStyle(
-                    fontSize: 20, color: Color.fromARGB(255, 255, 98, 140)),
-              ),
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.indigo,
+        ), //Icon es un widget especializado en mostrar iconos
+        onPressed: () {
+          print('Hola Mundo');
+        },
+      ),
+    );
   }
 }
